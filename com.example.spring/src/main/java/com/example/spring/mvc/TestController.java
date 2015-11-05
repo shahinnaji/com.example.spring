1eeff545d9dbb6edf40f521cb1b2ebaa2153ea12
shahin.naji <shahin.naji@samsung.com>
2015-11-05 15:56:27 -0800

    Adding slf4j with log4j implementation

--- com.example.spring/src/main/java/com/example/spring/mvc/TestController.java
+++ com.example.spring/src/main/java/com/example/spring/mvc/TestController.java
@@ -1,13 +1,23 @@
 package com.example.spring.mvc;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TestController {
 
+	private static final Logger logger = Logger.getLogger(TestController.class);
+	
 	@RequestMapping("/test")
 	public String test() {
+		final String message = "Hello logging!";
+        logger.trace(message);
+        logger.debug(message);
+        logger.info(message);
+        logger.warn(message);
+        logger.error(message);
+        
 		return "welcome";
 	}
 }
